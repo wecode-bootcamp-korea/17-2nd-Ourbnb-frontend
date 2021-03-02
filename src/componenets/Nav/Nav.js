364b9a08501875bea47e67814e4d0ed05f9f60a9
이름 <awaji0829@gmail.com>
2021-03-02 17:22:19 +0900

    초기세팅 수정완료

--- src/componenets/Nav/Nav.js
+++ src/componenets/Nav/Nav.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Nav = () => {
-  return <div>nav</div>;
+  return <Box>nav</Box>;
 };
 
 export default Nav;
+
+const Box = styled.div`
+  width: 100%;
+  background-color: blue;
+`;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GlobalStyles from 'styles/GlobalStyles';
+import GlobalStyles from './styles/GlobalStyles';
 import Routes from './Routes';
 
 ReactDOM.render(
