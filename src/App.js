8d9bbc7f8f9cf0853b35cd662c5e968c55f42561
이름 <awaji0829@gmail.com>
2021-03-02 16:25:56 +0900

    app.js 초기세팅 완료

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './Pages/Main/Main';
 import Nav from './componenets/Nav/Nav';
+import Login from './Pages/Login/Login';
+import Detailpage from './Pages/Detailpage/Detailpage';
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Nav />
       <Switch>
         <Route exact path="/" component={Main} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/detailpage" component={Detailpage} />
       </Switch>
     </Router>
   );
---
+++ src/Pages/Detailpage/Detailpage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Detailpage = () => {
+  return <div>혜성스 화이팅 디테일 페이지</div>;
+};
+
+export default Detailpage;
