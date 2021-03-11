7ebafd36f3781d0e6ae4c79435aa65934c3d5535
kimyeonju <duswn4338@gmail.com>
2021-03-11 16:28:07 +0900

    최종수정

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const URL = 'http://10.58.1.209:8000';
 export const APIKEY = 'AIzaSyDZ5_HuTJz3YdXQcbZBy-R0eKuNWWe2UoU';
-export const kakaoapi = 'http://192.168.0.6:8000/user/kakao/signin';
+export const kakaoapi = 'http://10.58.0.80:8000/user/kakao/signin';
 export const cardPayment = 'http://10.58.1.24:8000/reservation/purchase';
